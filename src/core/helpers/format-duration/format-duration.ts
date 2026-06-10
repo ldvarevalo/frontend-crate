1058de1aee82afe5f91ff1a7da1e9d99afa93d91
Luciana Arevalo <arevalolucianadg@gmail.com>
2026-06-09 23:33:20 -0300

    test: add formatDuration unit tests

--- src/core/helpers/format-duration/format-duration.ts
+++ src/core/helpers/format-duration/format-duration.ts
@@ -8,7 +8,7 @@ export const formatDuration = (seconds: number | null): string => {
   }
 
   const m = Math.floor(seconds / 60);
-  const s = seconds % 60;
+  const s = Math.floor(seconds % 60);
 
   return `${m}:${s.toString().padStart(2, '0')}`;
 };
